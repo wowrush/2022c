4a4ed9c2307fb980bc153cc52da8a6262a87e1a5
wowrush <114201752+wowrush@users.noreply.github.com>
2022-10-07 11:47:43 +0800

    week05

---
+++ week05-2s.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+   int a=5;
+   while(a>0) {
+        printf("a:%d a>0\n",a);
+        a--;
+   }
+}
---
+++ week05-3.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    for( int a=5 ; a>0 ; a--){
+        printf("a:%d a>0\n" , a );
+    }
+}
