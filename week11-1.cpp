9d5a3656b69b53ff671498f341dd2738f07e4858
unknown <wowrush>
2022-11-18 11:52:08 +0800

    修改目錄

--- week11-1.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h.>
-int main()
-{
-    int a=90, b=80;
-    int temp = a;
-    a=b;
-    b=temp;
-    printf("%d %d",a,b);
-}
---
+++ week11/week11-1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h.>
+int main()
+{
+    int a=90, b=80;
+    int temp = a;
+    a=b;
+    b=temp;
+    printf("%d %d",a,b);
+}
