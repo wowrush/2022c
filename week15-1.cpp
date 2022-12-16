82ddf76adf81f9f9a7d961e156bb1f7087b76aca
wowrush <114201752+wowrush@users.noreply.github.com>
2022-12-16 11:53:58 +0800

    week15

---
+++ week15-1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    char line[]="Hello World";
+    printf("整數 %d\n",100);
+    printf("浮點數 %f\n",3.141592684579);
+    printf("%s 字串\n",line);
+}
