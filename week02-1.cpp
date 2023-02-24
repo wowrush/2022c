4a30067a83e584918813c7c8bb30f422ed3c4033
wowrush <a0970317822@gmail.com>
2023-02-24 11:43:15 +0800

    week02

--- week02-1.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int a,b;
-	while(scanf("%d%d",&a,&b)==2){
-		int ans=b-a;
-		printf("%d\n",ans);
-	}
-}
---
+++ week02/week02-3.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	long long int a,b;
+	while(scanf("%lld%lld",&a,&b)==2){
+		long long int ans =b-a;
+		if(ans<0) ans =a-b;
+		printf("%lld\n",ans);
+	}
+}
