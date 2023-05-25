7766ca6c08464752cbe1a40a7a77af248269096b
wowrush <a0970317822@gmail.com>
2023-05-25 14:05:23 +0800

    week15

--- week13-3b.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int a;
-	scanf("%d",&a);
-	int c=60*60*1.2/a;
-	printf("%d",c);
-}
--- week13-3c.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int a,b;
-	scanf("%d%d",&a,&b);
-	if(a%b==0)printf("%d",a/b);
-	else printf("%d",a/b+1);
-}
